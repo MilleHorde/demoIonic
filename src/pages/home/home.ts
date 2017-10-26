9dcfa4d467d2f0460dcda6fa29a7ceb91d5843ae
nicolas <nicolas@gojob.com>
2017-10-26 15:05:45 +0200

    ez

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  app: any =  { name: String, version: Number };
+  app: any = {name: String, version: Number};
   img: string;
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, public camera: Camera) {
     this.app.name = "AppName";
     this.app.version = "3.0";
   }
@@ -17,17 +18,17 @@ export class HomePage {
   takePicture() {
     const options: CameraOptions = {
       quality: 100,
-      destinationType: Camera.DestinationType.DATA_URL,
-      encodingType: Camera.EncodingType.JPEG,
-      mediaType: Camera.MediaType.PICTURE
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
     };
 
-    Camera.getPicture(options).then((imageData) => {
+    this.camera.getPicture(options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
       this.img = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
-      // Handle error
+      console.log(err)
     });
   }
 
