388b54243c657bbcfeb909646b7b805b78d610d1
nicolas <nicolas@gojob.com>
2017-10-26 15:07:09 +0200

    ez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {Camera, CameraOptions} from '@ionic-native/camera';
+import {Camera} from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
